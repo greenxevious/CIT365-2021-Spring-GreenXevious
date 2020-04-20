af21173ad39c816b2f8117b96cf376745ef2342d
xevious <xeviousgreen@gmail.com>
2020-04-20 14:55:42 -0600

    working

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string name = "Xevious Green.";
+
+            string location = " San Antonio, Texas.";
+
+
+            DateTime thisDay = DateTime.Today;
+
+            Console.WriteLine("Todays date is " + thisDay.ToString("d"));
+
+            Console.WriteLine("My name is " + name);
+
+            Console.WriteLine("I am from" + location);
+
+
+        }
+
+
+
+    }
+}
