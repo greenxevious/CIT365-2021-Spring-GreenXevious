74430442d0cc881d94c1804dbb9a70cd044d97c0
xevious <xeviousgreen@gmail.com>
2020-04-20 14:25:25 -0600

    update

---
+++ ConsoleApplication.cs
@@ -0,0 +1,24 @@
+using System;
+class starter {
+    
+    
+    
+  static void Main() {
+      
+      string name = ("Xevious Green.");
+      
+      string location = " San Antonio, Texas.";
+      
+
+    Console.WriteLine("My name is " + name);
+    
+        Console.WriteLine("I am from"+ location);
+
+    
+  }
+  
+  
+  
+  
+  
+}
