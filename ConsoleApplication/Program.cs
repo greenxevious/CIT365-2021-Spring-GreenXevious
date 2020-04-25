29c1bafbb831aab899e8b4a8faf1b282a597b39c
xevious <xeviousgreen@gmail.com>
2020-04-25 03:18:00 -0600

    finish update

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -7,20 +7,43 @@ namespace ConsoleApplication
         static void Main()
         {
 
-            string name = "Xevious Green.";
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
 
-            string location = " San Antonio, Texas.";
+            Console.WriteLine("Where are you located?");
+            string location = Console.ReadLine();
 
+            DateTime today = DateTime.Today;
+            DateTime Day = today;
 
-            DateTime thisDay = DateTime.Today;
+            DateTime christmas = new DateTime(2020,12 ,25);
+             TimeSpan timeframe = christmas - Day;
 
-            Console.WriteLine("Todays date is " + thisDay.ToString("d"));
+            string total = $"My name is {name}." + $" I am from {location}.";
+            Console.WriteLine(total);
+            Console.WriteLine($"Todays date is {Day:d}.");
+            Console.WriteLine($"Time from today until Christmas is {timeframe.TotalDays} days away.");
 
-            Console.WriteLine("My name is " + name);
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
 
-            Console.WriteLine("I am from" + location);
+            Console.WriteLine("What is the width?");
 
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
 
+            Console.WriteLine("What is the height?");
+
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
 
 
